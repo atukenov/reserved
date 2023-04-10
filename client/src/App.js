89d1b31d3631b0f50ca79a6016e1c3597bfdf375
amakenzi <almaz.t97@gmail.com>
2023-04-10 23:47:03 +0500

    ui fix, redux added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./pages/Layout";
+import Home from "pages/home/Home";
+import RestaurantDetail from "components/restaurantDetail/RestaurantDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "restaurant/:restaurantId",
+        element: <RestaurantDetail />,
+      },
+    ],
   },
 ]);
 
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -3,13 +3,13 @@ import Footer from "components/footer/Footer";
 import Header from "components/header/Header";
 import Navbar from "components/navbar/Navbar";
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div>
       <Navbar />
-      <Header />
-      <Featured />
+      <Outlet />
       <Footer />
     </div>
   );
