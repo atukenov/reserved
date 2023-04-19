2f7f39ed5ed443083ead0269df16af6e4df2f6f5
amakenzi <almaz.t97@gmail.com>
2023-04-19 20:03:41 +0500

    only backend

--- client/src/react-app-env.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react-scripts" />
-GENERATE_SOURCEMAP = false;
--- client/src/router/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from "react";
-import { useRoutes } from "react-router-dom";
-import routes from "./config";
-
-const Router = () => {
-  return <Suspense fallback={null}>{useRoutes(routes)}</Suspense>;
-};
-
-export default Router;
