c14d647a4198cae07f22aca04ed9404fd4f4c181
atukenov <almaz.t97@gmail.com>
2023-04-23 03:32:47 +0500

    todo: mapping as grid

--- client/src/components/common/Mapping/index.tsx
+++ client/src/components/common/Mapping/index.tsx
@@ -1,35 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table, Map, Window, Enter } from "./styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import Container from "../Container";
 
+interface MapProps {
+  numRows: number;
+  numCols: number;
+}
+
 const Mapping = () => {
-  return (
-    <Container>
-      <Map height={600}>
-        <Window>windows</Window>
-        <Table top={50} left={15}>
-          Table 1
-        </Table>
-        <Table top={20} left={35}>
-          Table 2
-        </Table>
-        <Table top={35} left={65}>
-          Table 3
-        </Table>
-        <Table top={40} left={95}>
-          Table 4
-        </Table>
-        <Table top={55} left={125}>
-          Table 5
-        </Table>
-        <Enter>
-          <FontAwesomeIcon icon={faRightToBracket} />
-        </Enter>
-      </Map>
-    </Container>
+  const numRows = 12;
+  const numCols = 6;
+  const [mapData, setMapData] = useState<string[][]>(
+    Array.from({ length: numRows }, (_, i) =>
+      Array.from({ length: numCols }, (_, j) => `${i + 1}-${j + 1}`)
+    )
   );
+
+  useEffect(() => {}, [mapData]);
+
+  const handleCellClick = (rowIndex: number, colIndex: number) => {
+    const newMapData = mapData;
+    console.log(newMapData[rowIndex][colIndex]);
+    if (newMapData[rowIndex][colIndex] === "clicked")
+      newMapData[rowIndex][colIndex] = `${rowIndex + 1}-${colIndex + 1}`;
+    else newMapData[rowIndex][colIndex] = "clicked";
+    setMapData(newMapData);
+  };
+
+  return (
+    <Map>
+      <div className="map">
+        {mapData.map((row, i) => {
+          const cells = row.map((cellData, j) => {
+            return (
+              <div
+                key={`${i}-${j}`}
+                className="cell"
+                onClick={() => handleCellClick(i, j)}
+              >
+                {cellData}
+              </div>
+            );
+          });
+          return (
+            <div key={i} className="row">
+              {cells}
+            </div>
+          );
+        })}
+      </div>
+    </Map>
+  );
+
+  // return (
+  //   <Container>
+  //     <Map height={600}>
+  //       <Window>windows</Window>
+  //       <Table top={30} left={15}>
+  //         Table 1
+  //       </Table>
+  //       <Table top={30} left={80}>
+  //         Table 2
+  //       </Table>
+  //       <Table top={30} left={145}>
+  //         Table 3
+  //       </Table>
+  //       <Table top={30} left={210}>
+  //         Table 4
+  //       </Table>
+  //       <Table top={30} left={275}>
+  //         Table 5
+  //       </Table>
+  //       <Enter bottom={10} right={10}>
+  //         <FontAwesomeIcon icon={faRightToBracket} />
+  //       </Enter>
+  //     </Map>
+  //   </Container>
+  // );
 };
 
 export default Mapping;
--- client/src/components/common/Mapping/styles.ts
+++ client/src/components/common/Mapping/styles.ts
@@ -5,35 +5,73 @@ interface Props {
   height?: number;
   top?: number;
   left?: number;
+  bottom?: number;
+  right?: number;
   w?: number;
   h?: number;
 }
 
 export const Map = styled.div<Props>`
-  margin: auto;
-  margin-top: 20px;
   background-color: gray;
   max-width: 1200px;
-  width: 100%;
-  height: ${(p) => (p.height ? `${p.height}px` : "")};
+  /* width: 100%;
+  height: ${(p) => (p.height ? `${p.height}px` : "")}; */
   position: relative;
+  display: flex;
+  justify-content: center;
+
+  .map {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+  .row {
+    display: flex;
+  }
+
+  .cell {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1 1 100px;
+    height: 50px;
+    background-color: lightgray;
+    border: 1px solid gray;
+  }
 `;
 
 export const Window = styled.div<Props>``;
 export const Table = styled.div<Props>`
   position: absolute;
-  width: ${(p) => (p.w ? `${p.w}px` : "100px")};
-  height: ${(p) => (p.h ? `${p.h}px` : "50px")};
+  width: ${(p) => (p.w ? `${p.w}px` : "60px")};
+  height: ${(p) => (p.h ? `${p.h}px` : "30px")};
   background-color: rgba(240, 74, 204, 0.7);
   color: white;
 
   text-align: center;
   top: ${(p) => (p.top ? `${p.top}px` : "")};
   left: ${(p) => (p.left ? `${p.left}px` : "")};
+
+  @media ${device.tablet} {
+    width: ${(p) => (p.w ? `${p.w}px` : "80px")};
+    height: ${(p) => (p.h ? `${p.h}px` : "40px")};
+  }
+  @media ${device.laptop} {
+    width: ${(p) => (p.w ? `${p.w}px` : "100px")};
+    height: ${(p) => (p.h ? `${p.h}px` : "50px")};
+  }
+  @media ${device.desktop} {
+    width: ${(p) => (p.w ? `${p.w}px` : "100px")};
+    height: ${(p) => (p.h ? `${p.h}px` : "50px")};
+  }
 `;
 export const Enter = styled.div<Props>`
+  position: absolute;
   color: red;
   font-size: 30px;
   top: ${(p) => (p.top ? `${p.top}px` : "")};
   left: ${(p) => (p.left ? `${p.left}px` : "")};
+  right: ${(p) => (p.right ? `${p.right}px` : "")};
+  bottom: ${(p) => (p.bottom ? `${p.bottom}px` : "")};
 `;
