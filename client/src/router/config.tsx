c7dc9912d150ce6a0dac1b9d3619db92213c7632
amakenzi <almaz.t97@gmail.com>
2023-04-24 14:26:29 +0500

    mapping feature found

--- client/src/router/config.tsx
+++ client/src/router/config.tsx
@@ -37,7 +37,7 @@ const routes = [
       },
       {
         path: "map",
-        element: <Mapping />,
+        element: <Mapping mode="rect" />,
       },
       {
         path: "reservation/:reservationId",
